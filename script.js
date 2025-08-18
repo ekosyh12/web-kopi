9ac854ad433dd92e20c454cab81080c3a5f33c6e
ekosyh12 <ekosyh01@gmail.com>
2025-08-18 14:50:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// toggle class active untuk humberger menu
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  e.preventDefault();
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+};
+
+
+// toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  e.preventDefault(); // ✅ cegah scroll
+  shoppingCart.classList.toggle("active");
+};
+
+
+// klik di laur element
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
+
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+  shoppingCart.classList.remove("active");
+}
+
+
+});
